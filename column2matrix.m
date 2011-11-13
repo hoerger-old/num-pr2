a43fc3808bdafd2fbad0bd8aae3c83f40ae5f0b3
hanse <thehanse@gmail.com>
2011-11-13 01:52:35 +0100

    column2matrix code

--- column2matrix.m
+++ column2matrix.m
@@ -1,3 +1,9 @@
-function M=column2matrix(M)
-
+function N=column2matrix(M)
+    n=(-1+sqrt(1+8*length(M)))/2;
+    N=zeros(n,n);
+    w=1;
+    for i=1:n
+        N(i:n,i)=M(w:w+n-i);
+        w=w+(n-i+1);
+    end
 end
