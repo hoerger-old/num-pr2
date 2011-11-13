53892d9a63df6b41dedbda58c568aa3c070aa78d
hanse <thehanse@gmail.com>
2011-11-13 02:17:15 +0100

    made genBM working

--- genBM.m
+++ genBM.m
@@ -6,17 +6,17 @@ for i=n:-1:1
     for s=n:-1:1
         %A part
         M(w)=4; w=w+1;
-        if(s~=1)
+        if(s>1)
             M(w)=-1;
             w=w+1;
         end
-        if((s-2)>0), w=w+(s-2); end
+        if(s>2), w=w+(s-2); end
         %B part
         if(i>1)
             w=w+(n-s);
             M(w)=-1;
-            w=w+(s-1);
+            w=w+s;
         end
-        if((i-2)>0), w=w+(i-2)*n+1; end
+        if(i>2), w=w+((i-2)*n); end
     end
 end
