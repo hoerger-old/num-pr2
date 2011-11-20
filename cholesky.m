60d4b9dc0788574a6e497117f0e039aa7dd07fe8
hanse <thehanse@gmail.com>
2011-11-20 22:01:56 +0100

    cholesky works now

--- cholesky.m
+++ cholesky.m
@@ -11,7 +11,7 @@ function A=cholesky(A)
         for i=k:-1:2
             iw=iw+i;
             for j=0:i-2
-                A(iw+j)=A(iw+j)-A(s+j)*A(s+k-i+1);
+                A(iw+j)=A(iw+j)-A(s+(k-i)+j+1)*A(s+k-i+1);
             end
         end
         s=s+k;
