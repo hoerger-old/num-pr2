abc6a873c82f09c614347f64677f37c376d65f89
hanse <thehanse@gmail.com>
2011-11-24 14:56:55 +0100

    doku/typos behoben

--- cholesky.m
+++ cholesky.m
@@ -1,15 +1,23 @@
+% Eingabe: Symmetrische Matrix im Standardspaltenformat
+% Ausgabe: Untere Dreiecksmatrix im Standardspaltenformat
 function A=cholesky(A)
     l=length(A);
+    % Groesze der Matrix im klassischen Format
     n=(-1+sqrt(1+8*length(A)))/2;
+    % s speichert die Startposition der aktuellen Spalte
     s=1;
+    % umgekehrte Iteration ueber die Spalten um immer die
+    % Groesze der aktuellen Spalte zu kennen
     for k=n:-1:1
         A(s)=sqrt(A(s));
         for i=1:k-1
             A(s+i)=A(s+i)/A(s);
         end
         iw=s;
+        % Umgekehrte Iteration wie bei k
         for i=k:-1:2
             iw=iw+i;
+            % Indexverschiebung um Addition zu vereinfachen
             for j=0:i-2
                 A(iw+j)=A(iw+j)-A(s+(k-i)+j+1)*A(s+k-i+1);
             end
--- genBM.m
+++ genBM.m
@@ -2,16 +2,17 @@ function [M]=genBM(n)
 l=((n*n+1)*n*n)/2;
 M=zeros(1,l);
 w=1;
+% die Funktion geht Spaltenweise vor
 for i=n:-1:1
     for s=n:-1:1
-        %A part
+        % erzeuge eine Spalte von A
         M(w)=4; w=w+1;
         if(s>1)
             M(w)=-1;
             w=w+1;
         end
         if(s>2), w=w+(s-2); end
-        %B part
+        % erzeuge eine Spalte von B
         if(i>1)
             w=w+(n-s);
             M(w)=-1;
